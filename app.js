661f4f0fc149b00121edcec97a28812fae194ffa
Amadeusz <u22_amadeuszdomanski@zsp1.siedlce.pl>
2026-03-16 09:33:16 +0100

    1 commit

---
+++ app.js
@@ -0,0 +1,14 @@
+function zad2(){
+    var integer = document.getElementById('integer').value;
+    alert("Zaokraglenie w gore: " + Math.ceil(integer));
+}
+
+function zad3(){
+    var integer = document.getElementById('integer').value;
+    alert("Liczba eulera: " + Math.exp(integer));
+}
+
+function zad4(){
+    var integer = document.getElementById('integer').value;
+    alert("Zaokraglenie w dol: " + Math.floor(integer));
+}
